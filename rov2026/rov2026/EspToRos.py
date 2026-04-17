a4e968da21f819accb2f776f9f2c9df1d5e493f5
abdelrahman <abdelrahman.y.salah@gmail.com>
2026-04-17 21:12:18 +0200

    Readded the ros files

---
+++ rov2026/rov2026/EspToRos.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import socket
+import json
+
+LISTEN_IP = "0.0.0.0"
+LISTEN_PORT = 8888
+
+class EspToRos(Node):
+    def __init__(self):
+        super().__init__('esp_to_ros')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((LISTEN_IP, LISTEN_PORT))
+        self.sock.setblocking(False) 
+        self.telemetry = self.create_publisher(String, 'rov_telemetry', 10)
+        self.timer = self.create_timer(0.01, self.check_udp_socket)
+        self.get_logger().info(f"Receiver Started. Listening on Port {LISTEN_PORT}")
+
+    def check_udp_socket(self):
+        try:
+            data, addr = self.sock.recvfrom(1024)
+
+            if data:
+                raw_text = data.decode('utf-8').strip()
+                try:
+                    values = raw_text.split(',')
+                    telemetry_dict = {
+                        "pitch": float(values[0]),
+                        "roll": float(values[1]),
+                        "yaw": float(values[2]),
+                        "depth": float(values[3])
+                    }
+                    msg = String()
+                    msg.data = json.dumps(telemetry_dict)
+                    self.telemetry.publish(msg)
+
+                except (ValueError, IndexError) as parse_error:
+                    self.get_logger().warn(f"Failed to parse data '{raw_text}': {parse_error}")
+
+        except BlockingIOError:
+            pass
+        except Exception as e:
+            self.get_logger().error(f"Socket Error: {e}")
+
+            json_msg = json.dumps(telemetry_dict)
+            msg = String()
+            msg.data = json_msg
+            self.telemetry.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = EspToRos()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.sock.close()
+        node.destroy_node()
+        rclpy.shutdown()
---
+++ rov2026/rov2026/gripper_controller.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String, Int32MultiArray
+import json
+
+class GripperController(Node):
+    def __init__(self):
+        super().__init__('gripper_controller')
+
+        self.BTN_GRIPPER_TOGGLE = 0
+
+        self.BTN_GRIPPER_SERVO = 2
+
+        self.gripper_value = 0
+        self.servo_value = 0
+
+        self.prev_buttons = []
+
+
+        self.create_subscription(String, 'joy_raw', self.joy_callback, 10)
+        self.gripper_pub = self.create_publisher(Int32MultiArray, 'gripper_cmd', 10)
+
+        self.get_logger().info("Gripper Node Started")
+    
+    def joy_callback(self, msg):
+
+        try:
+            data = json.loads(msg.data)
+            buttons = data.get('buttons', [])
+            
+            if buttons[self.BTN_GRIPPER_TOGGLE] == 1 and (not self.prev_buttons or self.prev_buttons[self.BTN_GRIPPER_TOGGLE] == 0):
+                self.get_logger().info("Gripper TOGGLE")
+                self.gripper_value = 1 - self.gripper_value
+
+            if buttons[self.BTN_GRIPPER_SERVO] == 1:
+                self.get_logger().info("Gripper SERVO")
+                self.servo_value = 1700
+
+            else:
+                self.servo_value = 1500
+
+            self.prev_buttons = buttons
+
+
+            gripper_msg = Int32MultiArray()
+            gripper_msg.data = [self.servo_value, self.gripper_value]
+            self.gripper_pub.publish(gripper_msg)
+
+        except json.JSONDecodeError:
+            self.get_logger().error("Failed to decode JSON from joy_raw message")
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = GripperController()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()    
